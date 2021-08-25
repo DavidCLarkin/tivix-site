d86b1c3485b696186b72023426b47b9174107db8
David Larkin <davidlarkin96@gmail.com>
2021-08-25 10:14:45 -0400

    implemented data tracker functions

--- src/pages/components/weather.js
+++ src/pages/components/weather.js
@@ -1,41 +1,58 @@
 import React, { useState, useEffect } from "react";
 import Utils from "../helpers/helpers.js";
 
-const Weather = data => {
+const Weather = () => {
 
+  const [location, setLocation] = useState("New York");
+  const [value, setValue] = useState("");
+  const [data, setData] = useState("");
   const [mins, setMins] = useState([]);
-  const [maxes, setMaxes] = useState([]);
-  const [mean, setMean] = useState([]);
-  const [mode, setMode] = useState([]);
+
+
+  async function fetchData() {
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${process.env.GATSBY_WEATHER_API_KEY}`)
+    .then(results => results.json())
+    .then(data => setData(data))
+    .then(data => {sessionStorage.setItem("WeatherData", data)})
+    .catch(err => console.log(err))
+  }
 
   function filterByTime(time) {
-    var date = Utils.convertUNIX(time)
+    let date = Utils.convertUNIX(time)
     return date.getUTCHours()
   } 
 
-  useEffect(() => {
-    function setTrackerData() {
-      data.data.list && data.data.list
-      .filter(weather => filterByTime(weather.dt) === 15)
-      .map(item => {
-        //setMins([...mins, item.main.temp_min]);
-        console.log(item.main.temp_min);
-      })
-    }
+  function changeLocation(event) {
+    setLocation(event.target.value);
+  }
 
-    setTrackerData();
+  function changeValue(event) {
+    let newVal = event.target.value;
+    if(newVal != null || newVal != undefined) setValue(newVal);
+  }
+
+  function insertValue(value) {
+    if(value) setMins([...mins, value]);
     console.log(mins);
-  })
-
+  }
+  
   return <div>
     <div>
+      <form id="weatherForm">
+        <input type="text" value={location} onChange={changeLocation}></input>
+      </form>
+      <button type="submit" form="weatherForm" value="Fetch" onClick={(e) => {
+        e.preventDefault();
+        fetchData();
+      }}>Fetch</button>
+      {console.log(Utils.showMode([1,1,2,3,4,4,4]))}
       <ol>
-        {data.data.list && data.data.list
+        {data.list && data.list
           .filter(weather => filterByTime(weather.dt) === 15)
           .map(item => {
-            //console.log(item);
+            console.log(item);
             //setMins(e => [...e, item.main.temp_min])
-            return <li>
+            return <li key={item.dt}>
               <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} />
               <p>{Utils.convertUNIX(item.dt).toLocaleDateString("en-US", {weekday: "long", month: "long", day:"numeric"})}</p>
               <p>{Utils.kelvinToCelsius(item.main.temp)}°C</p>
@@ -45,6 +62,8 @@ const Weather = data => {
         }
       </ol>
       <div>
+        <input type="number" value={value} onChange={changeValue}></input>
+        <button onClick={() => insertValue(value)}>Insert Value</button>
         <p>Min Temp: </p>
         <p>Max Temp: </p>
         <p>Mean Temp: </p>
--- src/pages/helpers/helpers.js
+++ src/pages/helpers/helpers.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 
 export default class Helpers {
   static convertUNIX(unixTime) {
@@ -10,5 +11,43 @@ export default class Helpers {
 
   static kelvinToFahrenheit(kelvinTemp) {
     return Math.round((kelvinTemp - 273) * 9 / 5 + 32)
+  }
+
+  static showMin(arr) {
+    return _.min(arr);
+  }
+
+  static showMax(arr) {
+    return _.max(arr);
+  }
+
+  static showMean(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+      sum+=arr[i];
+    }
+    return sum / arr.length;
+  }
+
+  static showMode(arr) {
+    let max = 0, frequency = {}, highestKey = 0;
+    arr.sort();
+    for(let i = 0; i < arr.length; i++) {
+      if(!frequency[arr[i]]) {
+        frequency[arr[i]] = 1;
+      } else {
+        frequency[arr[i]] += 1;
+      }
+    }
+
+    for(let key in frequency) {
+      const val = frequency[key];
+      if(val > max) {
+        max = val;
+        highestKey = key;
+      }
+    }
+    
+    return highestKey;
   }
 }
