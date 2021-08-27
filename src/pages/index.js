a0cf02d43710a6cb335ae07a6fd8a70dad3b24b0
David Larkin <davidlarkin96@gmail.com>
2021-08-27 12:36:43 -0400

    more styling and removed background images.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Weather from "./components/weather.js";
 import "./css/base.scss";
-import bg1 from "../assets/images/tabbied1.png";
 
 export default function Home() {
   return <main className="main">
@@ -9,8 +8,6 @@ export default function Home() {
       <div className="appNameDiv">
         <h1 className="appName">Weather App</h1>
       </div>
-      <img className="bg" src={bg1} alt="bgOrbs"/>
-      <img className="bg2" src={bg1} alt="bgOrbs"/>
       <Weather/>
     </div>
     <footer></footer>
